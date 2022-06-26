62eb36bbd0d95e1162889ae4adb6a68ac6c57a39
Seiya_Satoh <satohseiya0312@gmail.com>
2022-06-26 23:31:54 +0900

    pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { of, Observable, Observer } from 'rxjs';
+import { of, Observable, Observer, filter, map, interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +8,26 @@ import { of, Observable, Observer } from 'rxjs';
 })
 export class AppComponent {
   title = 'CodeRepositoryOfAngular';
+
 }
 
-const observable = of(1, 2, 3);
+// const observable = of(1, 2, 3);
 
-const observer: Observer<number> = {
-  next: (value) => console.log(`next:${value}`),
-  error: (err) => console.log(err),
-  complete: () => console.log("complete"),
-}
+// const observer: Observer<number> = {
+//   next: (value) => console.log(`next:${value}`),
+//   error: (err) => console.log(err),
+//   complete: () => console.log("complete"),
+// }
 
-observable.subscribe(observer);
+// observable.subscribe(observer);
+
+of(1, 2, 3, 4, 5)
+  .pipe(
+    filter(x => x % 2 == 0),
+    map(x => x * x))
+  .subscribe((x) => console.log(x));
+
+const observable = interval(1000);
+observable.subscribe(x => console.log(x));
+
+
